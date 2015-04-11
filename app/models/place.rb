5176f25c1a086ee51b9ec4d8ae614798237c17de
Lucian Kahn <luciankahn@gmail.com>
2015-04-10 23:33:43 -0400

    outline of api call method

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ActiveRecord::Base
   geocoded_by :address
-  # before_save :api_call
+  before_save :api_call('https://data.cityofnewyork.us/resource/ujsc-un6m.json')
 
   validates :name, presence: true
   validates :category, presence: true
@@ -8,9 +8,13 @@ class Place < ActiveRecord::Base
   validates :latitude, presence: true, numericality: true
   validates :longitude, presence: true, numericality: true
 
+  def self.api_call(resource_url)
+    http_response = open(resource_url)
+    resource = JSON.parse(http_response.read)
+  end
 
-  # def api_call
+  def self.assign_homeless_resources
 
-  # end
+  end
 
 end

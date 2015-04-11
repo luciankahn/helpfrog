f70150347d9af8b5b849625b6f086ff4107276b1
Lucian Kahn <luciankahn@gmail.com>
2015-04-11 15:14:29 -0400

    extract socrata_parse method to handle all nyc open data queries

--- db/update_apis.rb
+++ db/update_apis.rb
@@ -1,15 +1,24 @@
 # Run this rake task once a month to get new resources.
 
-# After School Programs for Runaway and Homeless Youth
-# NYC Open Data Portal
-response = open('https://data.cityofnewyork.us/resource/ujsc-un6m.json')
-homeless_youth_collection = JSON.parse(response.read)
-homeless_youth_collection.each do |site|
-  name = site["site_name"]
-  category = :youth
-  address = site["location_1"]["human_address"]
-  phone_number = site["contact_number"]
-  latitude = site["location_1"]["latitude"]
-  longitude = site["location_1"]["longitude"]
-  Place.create_with(category: category, address: address, phone_number: phone_number, latitude: latitude, longitude: longitude).find_or_create_by(name: name)
+# Handles NYC OpenData queries
+def socrata_parse(json_url, category)
+  response = open(json_url)
+  collection = JSON.parse(response.read)
+  collection.each do |site|
+    name = site["site_name"]
+    address = site["location_1"]["human_address"]
+    phone_number = site["contact_number"]
+    latitude = site["location_1"]["latitude"]
+    longitude = site["location_1"]["longitude"]
+    Place.create_with(category: category, address: address, phone_number: phone_number, latitude: latitude, longitude: longitude).find_or_create_by(name: name)
+  end
 end
+
+# DYCD After School Programs: Housing
+socrata_parse('https://data.cityofnewyork.us/resource/fqcv-e9sg.json', :housing)
+
+# NEED ERROR HANDLING for missing columns, so the rake doesn't break.
+# After School Programs for Runaway and Homeless Youth
+socrata_parse('https://data.cityofnewyork.us/resource/ujsc-un6m.json', :youth)
+
+

099b8e5f3aaa9ba02311f1b4f02047933acf59da
Lucian Kahn <luciankahn@gmail.com>
2015-04-12 15:10:30 -0400

    create page for alphabetical list of all locations in database

--- app/controllers/places.rb
+++ app/controllers/places.rb
@@ -1,6 +1,14 @@
+# display an alphabetical list of all places (by name), as links to each place's page
+get '/places' do
+  @places = Place.all.sort_by { |place| place.name }
+  erb :'/places/show_all'
+end
+
 # display 1 place and all of its comments
 get '/places/:id' do |id|
   @place = Place.find(id)
   @comments = @place.comments
   erb :'/places/show'
 end
+
+

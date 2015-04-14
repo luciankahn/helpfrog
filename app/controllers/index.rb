7fc1a6315cf107207357254082bfeeaa6c0e3ae5
Lucian Kahn <luciankahn@gmail.com>
2015-04-13 22:13:56 -0400

    mark non-functioning code in index.rb for filtering checkboxes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,17 @@
 get '/' do
   erb :'index'
 end
+
+post '/' do
+  if params['address'].nil?
+    redirect '/'
+  else
+    @address = params['address']
+    if params['filter'].nil?
+      @filter = Category.all.name # This isn't getting assigned.
+    else
+      @filter = params['filter']  # This isn't getting assigned properly from index.erb.
+    end
+    erb :'/results'
+  end
+end

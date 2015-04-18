c09c046ad086ed9b12cca2b1f5df59ee1dcac715
Lucian Kahn <luciankahn@gmail.com>
2015-04-18 18:42:48 -0400

    troubleshooting xhr

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -3,12 +3,11 @@ get '/places/:id/comments' do |id|
 end
 
 post '/places/:id/comments' do |id|
+  place = Place.find(id)
+  place.comments << Comment.create(params[:comment])
   if request.xhr?
-    # something goes here to find the new comment
-    erb: 'places/_new-comment', locals: {comment: @comment}
+    erb :'places/_comment', locals: {comment: @comment}
   else
-    place = Place.find(id)
-    place.comments << Comment.create(params[:comment])
     redirect "/places/#{id}"
   end
 end

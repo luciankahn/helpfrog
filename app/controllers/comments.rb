6efc5c4b6f4989ec5c5e7ad8bc82ccde8e666050
Lucian Kahn <luciankahn@gmail.com>
2015-04-18 17:06:45 -0400

    non-ajax version of adding comments is working

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,3 +1,9 @@
-post '/places/:id/comments' do |id|
+get '/places/:id/comments' do |id|
+  redirect "/places/#{id}"
+end
 
+post '/places/:id/comments' do |id|
+  place = Place.find(id)
+  place.comments << Comment.create(params[:comment])
+  redirect "/places/#{id}" # non-AJAX version
 end

3050322dce20b7667a5d040280ce5f98ce1b4162
Lucian Kahn <luciankahn@gmail.com>
2015-04-18 17:59:09 -0400

    extract single comment into _comment.erb

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -2,8 +2,9 @@ get '/places/:id/comments' do |id|
   redirect "/places/#{id}"
 end
 
+# non-AJAX version
 post '/places/:id/comments' do |id|
   place = Place.find(id)
   place.comments << Comment.create(params[:comment])
-  redirect "/places/#{id}" # non-AJAX version
+  redirect "/places/#{id}"
 end

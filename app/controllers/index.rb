8e7c38c4db255f660bf6228c825342ff3e3f57e1
Lucian Kahn <luciankahn@gmail.com>
2015-04-18 21:44:19 -0400

    fix checkboxes with [] in the name to get all selected boxes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,11 +7,12 @@ post '/' do
     redirect '/'
   else
     @address = params['address']
-    if params['filter'].nil?
+    if params['filter'] == ""
       @filter = Category.all.name # This isn't getting assigned.
     else
       @filter = params['filter']  # This isn't getting assigned properly from index.erb.
     end
+    puts "Filter: #{params['filter']}"
     erb :'/results'
   end
 end

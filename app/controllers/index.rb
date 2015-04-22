bac57d1c4722fad891714650b4fc7e631af902ed
Lucian Kahn <luciankahn@gmail.com>
2015-04-21 21:41:50 -0400

    refactoring results route to prepare for an ajax call

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -13,14 +13,14 @@ get '/results' do
       filter_categories = params['filter']
     end
     @places = Place.joins(:categories).near(@address).where("categories.name IN (?)", filter_categories).limit(30)
+  end
+  if request.xhr?
     @lat_longs = []
     @places.each do |place|
       @lat_longs << [place.latitude, place.longitude]
     end
     p @lat_longs.to_json
+  else
     erb :'/results'
-    # google_maps.js needs to receive the @lat_longs json file in order to iterate through it and construct marker objects for the map on results.erb.
-    # However, the list that appears below the map in results.erb needs @places in order to render. And results.erb needs to render as a result of this route.
-    # How do I pass @lat_longs.to_json to google_maps.js AND @places to results.erb, AND render results.erb, from this route?
   end
 end

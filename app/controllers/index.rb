4337059a949de22cf2966deb1e870a3cd3da7e58
Lucian Kahn <luciankahn@gmail.com>
2015-04-22 14:06:33 -0400

    @lat_longs is correctly populated

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -13,14 +13,11 @@ get '/results' do
     filter_categories = params['filter']
   end
   @places = Place.joins(:categories).near(@address).where("categories.name IN (?)", filter_categories).limit(30)
-  # debug this. my ajax call in application.js is succeeding but receiving an empty array.
   if request.xhr?
-    p @places
     @lat_longs = []
     @places.each do |place|
       @lat_longs << place.latitude
     end
-    puts "Here is @lat_longs: #{@lat_longs}"
     @lat_longs.to_json
   else
     erb :'/results'

156fbf7e9f1260e8c7a7e1bbaf3b8a46ecd0ee7a
Lucian Kahn <luciankahn@gmail.com>
2015-04-24 16:01:53 -0400

    add categories resource, route, controller. add migrations for categories, places, comments

--- rails/helpfrog/config/routes.rb
+++ rails/helpfrog/config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :places
+  resources :categories
   resources :comments
 
   # Example resource route with options:

378bb3f6e9e0cf3216e8daeaac08ff18a95ef254
Lucian Kahn <luciankahn@gmail.com>
2015-04-24 16:07:21 -0400

    migrations for comments, places, categories

--- rails/helpfrog/db/migrate/20150424195904_create_places.rb
+++ rails/helpfrog/db/migrate/20150424195904_create_places.rb
@@ -1,6 +1,14 @@
 class CreatePlaces < ActiveRecord::Migration
   def change
     create_table :places do |t|
+      t.string :name, null: false
+      t.string :phone_number
+      t.string :address, null: false
+      t.float :latitude, null: false, numeric: true
+      t.float :longitude, null: false, numeric: true
+      t.string :description
+
+      t.timestamps
     end
   end
 end
--- rails/helpfrog/db/migrate/20150424195917_create_comments.rb
+++ rails/helpfrog/db/migrate/20150424195917_create_comments.rb
@@ -1,6 +1,13 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.string :title
+      t.string :content, null: false
+      t.integer :rating
+      t.string :password
+      t.references :place
+
+      t.timestamps
     end
   end
 end

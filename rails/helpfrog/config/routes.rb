fcbcfbbae8de90faffc92c9100ddb299a4b2c9c3
Lucian Kahn <luciankahn@gmail.com>
2015-04-26 11:49:36 -0400

    write get routes for show all places, show one place, show results, show all places in 1 category. no controllers yet

--- rails/helpfrog/config/routes.rb
+++ rails/helpfrog/config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  get '/places' => 'places#show_all'
+  get '/places/:id' => 'places#show'
+  get '/results' => 'results#show'
+  get '/categories/:id' => 'categories#show'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 

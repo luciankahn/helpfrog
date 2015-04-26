e523232520a8682b4a163ecb5fa46da111a5640f
Lucian Kahn <luciankahn@gmail.com>
2015-04-26 11:05:34 -0400

    configure welcome route, controller, view. index is displaying but not connected to css

--- rails/helpfrog/db/schema.rb
+++ rails/helpfrog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424200105) do
+ActiveRecord::Schema.define(version: 20150425191732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20150424200105) do
     t.integer  "place_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "place_categories", force: :cascade do |t|
+    t.integer  "place_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "places", force: :cascade do |t|

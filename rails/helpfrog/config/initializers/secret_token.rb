937eb61ec5e422487b04e13af351e0a5faacff37
Lucian Kahn <luciankahn@gmail.com>
2015-04-27 19:19:09 -0400

    generate new rails app, set database.yml to postgres settings

---
+++ rails/helpfrog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helpfrog::Application.config.secret_token = 'fc1d583deec78dd33b24a596f039d4441d8104a24a4f1ed17ad97b9334e800175ec867b7770a0f064b22f7d26b5f3450b9a2524e82384e0eabdae22d4a641d19'

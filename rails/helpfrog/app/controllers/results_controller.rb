4c4ecf69494bc26c2f006bb6ee5367dfa1e8ccf3
Lucian Kahn <luciankahn@gmail.com>
2015-04-27 22:15:03 -0400

    search form working, rendering results on results page

--- rails/helpfrog/app/controllers/results_controller.rb
+++ rails/helpfrog/app/controllers/results_controller.rb
@@ -1,6 +1,22 @@
 class ResultsController < ApplicationController
 
   def show
+    @address = params['address']
+    if params['filter'] == ""
+      filter_categories = Category.all.name
+    else
+      filter_categories = params['filter']
+    end
+    @places = Place.joins(:categories).near(@address).where("categories.name IN (?)", filter_categories).limit(30)
+    # if request.xhr?
+    #   @lat_longs = []
+    #   @places.each do |place|
+    #     @lat_longs << [place.latitude, place.longitude, place.name]
+    #   end
+    #   @lat_longs.to_json
+    # else
+    #   erb :'/results'
+    # end
   end
 
 end
